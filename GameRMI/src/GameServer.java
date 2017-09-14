b75f53804cfbdb27d8d888fb593a01db5d0b4576
Dmitrii Bainak <mr.baynak@gmail.com>
2017-09-14 17:14:14 +0300

    Update README.md

--- GameRMI/src/GameServer.java
+++ GameRMI/src/GameServer.java
@@ -1,29 +1,29 @@
 
-import java.rmi.Naming; 
-import java.rmi.registry.LocateRegistry; 
-import java.rmi.server.UnicastRemoteObject; 
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class GameServer 
+public class GameServer
 {
-    public static void main (String[] argv) 
+    public static void main (String[] argv)
     {
-               
-        try 
+
+        try
         {
-            // Instantiating the implementation class 
-            TheGame game = new TheGame(); 
+            // Instantiating the implementation class
+            TheGame game = new TheGame();
 
-            // Exporting the object of implementation class  
-            // (here we are exporting the remote object to the stub) 
-            GameInterface stub = (GameInterface) UnicastRemoteObject.exportObject(game, 0);  
+            // Exporting the object of implementation class
+            // (here we are exporting the remote object to the stub)
+            GameInterface stub = (GameInterface) UnicastRemoteObject.exportObject(game, 0);
 
-            // Binding the remote object (stub) in the registry 
-            LocateRegistry.createRegistry(2020);  
+            // Binding the remote object (stub) in the registry
+            LocateRegistry.createRegistry(2020);
             Naming.rebind("//localhost:2020/RemoteGame", stub);
 
             System.out.println("The game server is ready.");
-            
-        }catch (Exception e) 
+
+        }catch (Exception e)
         {
             System.out.println("The game server failed to start: " + e);
             e.printStackTrace();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# JAVA-RMI-Weather-Service
+# JAVA RMI Weather Service
+
+This is a trivial RMI game created during "Distributed Programming" course.
+The server randomizes a number. Then clients are trying to find out which number was randomized. Callbacks to clients are implemented via shared interface of a client.
